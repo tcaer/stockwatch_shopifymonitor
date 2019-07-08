45bbd8e2a1d91266c48881526566828347bcffdb
Tino Caer <tino.caer@me.com>
2019-07-07 23:49:40 -0400

    Sass support for dev and build

--- packages/web/build/webpack.dev.js
+++ packages/web/build/webpack.dev.js
@@ -7,6 +7,18 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
     compress: true,
--- packages/web/build/webpack.production.js
+++ packages/web/build/webpack.production.js
@@ -1,12 +1,30 @@
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   optimization: {
     minimizer: [new UglifyJsPlugin()]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
 });
