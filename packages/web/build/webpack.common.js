f2dd5153bbe145e0b2a4d83a589bcf8d53185adc
Tino Caer <tino.caer@me.com>
2019-07-08 00:05:57 -0400

    More optimized asset loading, images, fonts, svgs

--- packages/web/build/webpack.common.js
+++ packages/web/build/webpack.common.js
@@ -22,17 +22,56 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff2|jpg|jpeg|png|svg)(\?.*)?$/,
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.svg$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'svg-url-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'dist/fonts'
-            }
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024,
+              noquotes: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                enabled: false,
+                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+                // Try enabling it in your environment by switching the config to:
+                // enabled: true,
+                // progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+            },
           }
         ]
-      },
+      }
     ]
   },
   plugins: [
